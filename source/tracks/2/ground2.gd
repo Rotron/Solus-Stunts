99bf5ca20c12521f4660d34bdb1f296ccc58c6ee
LINUXUSERGD <no-ads@mail.de>
2018-09-03 20:55:24 +0200

    add voice

--- source/tracks/2/ground2.gd
+++ source/tracks/2/ground2.gd
@@ -1,34 +1,45 @@
 extends Area
 var ROUND = 1
+var round2 = preload("res://assets/voice/round_2.ogg")
+var finalround = preload("res://assets/voice/final_round.ogg")
+var gameover = preload("res://assets/voice/game_over.ogg")
+var winner = preload("res://assets/voice/winner.ogg")
+
 
 func _ready():
 	pass 
 
-
+func game_over():
+	print("Game Over")
+	get_tree().get_root().get_node("lobby/world").audioend()
+	$"/root/lobby/world/countdown/audio".stream = gameover
+	$"/root/lobby/world/countdown/audio".play()
 
 func _on_StaticBody2_body_entered(_body):
-	print("Game Over")
-	get_tree().change_scene("res://lobby.tscn")
+	game_over()
 
 
 func _on_Area_body_entered(_body):
-	print("Game Over")
-	get_tree().change_scene("res://lobby.tscn")
-
-
-
+	game_over()
 
 func _on_goal2_body_entered(body):
+	nextround()
+	
+func nextround():
 	if ROUND < 3:
 		ROUND += 1
 		$"/root/lobby/world/round".text="round " + str(ROUND) + " of 3"
+		if ROUND == 2:
+			$"/root/lobby/world/countdown/audio".stream = round2
+		if ROUND == 3:
+			$"/root/lobby/world/countdown/audio".stream = finalround
+			
+		$"/root/lobby/world/countdown/audio".play()
 	else:
-		get_tree().change_scene("res://lobby.tscn")
-
+		get_tree().get_root().get_node("lobby/world").audioend()
+		$"/root/lobby/world/countdown/audio".stream = winner
+		$"/root/lobby/world/countdown/audio".play()
 
 func _on_goal_body_entered(body):
-	if ROUND < 3:
-		ROUND += 1
-		$"/root/lobby/world/round".text="round " + str(ROUND) + " of 3"
-	else:
-		get_tree().change_scene("res://lobby.tscn")
+	nextround()
+	
